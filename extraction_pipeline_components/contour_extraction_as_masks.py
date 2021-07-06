5aa294f67d963b771cfb09aa3b6fe5384ce18851
Samuel Ouellet <samuel.ouellet.10@ulaval.ca>
2021-07-06 11:01:03 -0400

    end of the contour extraction modules

--- extraction_pipeline_components/contour_extraction_as_masks.py
+++ extraction_pipeline_components/contour_extraction_as_masks.py
@@ -1,4 +1,6 @@
 import logging
+from typing import List, Tuple
+
 from extraction_pipeline_components.mask_structure_classes import Structures, Mask, SliceMask
 import numpy as np
 import pydicom
@@ -8,7 +10,17 @@ from extraction_pipeline_components.search_instance_and_convert_coord_in_pixel i
     extract_positionning_informations, find_instance_in_folder
 
 
-def extract_masks_for_each_organs_for_each_slices(rt_struct_file_path, study_folder):
+def extract_masks_for_each_organs_for_each_slices(rt_struct_file_path: str, study_folder: str) -> Structures or None:
+    """
+    This method will extract all information relative to the contours stored in the dicom rt struct.
+    In order to keep all the information organized, a Structure object will be created along with
+    all the Mask and SliceMask objects required to contain all the information
+
+    :param rt_struct_file_path: path to the RTSTRUCT Diocm containing the desired rois
+    :param study_folder: path to the folder containing the whole study
+
+    :return: a Structure object containing all contours
+    """
     open_dicom = pydicom.dcmread(rt_struct_file_path)
     try:
         if open_dicom.Modality == "RTSTRUCT":
@@ -48,23 +60,36 @@ def extract_masks_for_each_organs_for_each_slices(rt_struct_file_path, study_fol
             return structure
 
         logging.warning(f"")
-        return {}
+        return None
 
     except KeyError:
         logging.warning(f"")
-        return {}
+        return None
 
 
-def extract_contour_context_info(json_dict_of_dicom_rt_struct):
+def extract_contour_context_info(json_dict_of_dicom_rt_struct: dict) -> dict:
+    """
+    This method will extract the roi name and the obseration label of every contour
+
+    :param json_dict_of_dicom_rt_struct: json_dit associated to the original Dicom
+
+    :return: a dictionary containing both roi name and observation label for each contour
+    """
     structure_sequence = json_dict_of_dicom_rt_struct["30060020"]["Value"]
     roi_name_dict = {}
     for roi in structure_sequence:
-        roi_name_dict[roi["30060022"]["Value"][0]] = {"ROIName": roi["30060026"]["Value"][0]}
+        try:
+            roi_name_dict[roi["30060022"]["Value"][0]] = {"ROIName": roi["30060026"]["Value"][0]}
+        except KeyError:
+            roi_name_dict[roi["30060022"]["Value"][0]] = {"ROIName": ""}
 
     observation_sequence = json_dict_of_dicom_rt_struct["30060080"]["Value"]
     observation_label = {}
     for roi in observation_sequence:
-        observation_label[roi["30060084"]["Value"][0]] = {"ROIObservationLabel": roi["30060085"]["Value"][0]}
+        try:
+            observation_label[roi["30060084"]["Value"][0]] = {"ROIObservationLabel": roi["30060085"]["Value"][0]}
+        except KeyError:
+            observation_label[roi["30060084"]["Value"][0]] = {"ROIObservationLabel": ""}
 
     for roi in roi_name_dict.keys():
         roi_name_dict[roi].update(observation_label[roi])
@@ -72,8 +97,22 @@ def extract_contour_context_info(json_dict_of_dicom_rt_struct):
     return roi_name_dict
 
 
-def extract_contour_mask_and_image(json_dict_of_dicom_rt_struct, img_shape, x_y_z_spacing,
-                                   x_y_z_origin, x_y_z_rotation_vectors):
+def extract_contour_mask_and_image(json_dict_of_dicom_rt_struct: dict, img_shape: Tuple[int, int, int],
+                                   x_y_z_spacing: Tuple[float, float, float], x_y_z_origin: List[float],
+                                   x_y_z_rotation_vectors: List[float]) -> dict:
+    """
+    This method will use the rt struct json dict to extrac all of the masks and
+    associated image uid. The output of this method will be in pixel coordinates.
+
+    :param json_dict_of_dicom_rt_struct: json_dit associated to the original Dicom
+    :param img_shape: shape of the 3d image
+    :param x_y_z_spacing: spacial dimension of a voxel
+    :param x_y_z_origin: origin of the image in patient coordinates
+    :param x_y_z_rotation_vectors: image axes in the patient coordinates
+
+    :return:a dictionary containing both mask and image uid for each contour
+    """
+
     roi_contour_sequence = json_dict_of_dicom_rt_struct["30060039"]["Value"]
     roi_contours_dict = {}
     for roi in roi_contour_sequence:
@@ -85,10 +124,11 @@ def extract_contour_mask_and_image(json_dict_of_dicom_rt_struct, img_shape, x_y_
                 contour_data = np.asarray(slices["30060050"]["Value"], dtype=np.float64)
                 data_array = contour_data.reshape((contour_data.shape[0] // 3, 3))
                 slice_z = convert_real_coord_to_pixel_coord(np.asarray([data_array[0]]), x_y_z_spacing,
-                                                        x_y_z_origin, x_y_z_rotation_vectors)[0, 2]
+                                                            x_y_z_origin, x_y_z_rotation_vectors)[0, 2]
 
                 pixel_tuples = convert_real_coordinates_into_pixel_tuple_coordinates(data_array, x_y_z_spacing,
-                                                                                 x_y_z_origin, x_y_z_rotation_vectors)
+                                                                                     x_y_z_origin,
+                                                                                     x_y_z_rotation_vectors)
                 mask = produce_mask_from_contour_coord(pixel_tuples, (img_shape[1], img_shape[2]))
                 image_uid = slices["30060016"]["Value"][0]["00081155"]["Value"][0]
                 image = image_uid
@@ -96,13 +136,26 @@ def extract_contour_mask_and_image(json_dict_of_dicom_rt_struct, img_shape, x_y_
 
             roi_contours_dict[roi_number] = slices_dict
         except KeyError:
-            logging.warning("")
+            logging.warning(f"The roi {roi_number} does not have any images associated or something went wrong")
 
     return roi_contours_dict
 
 
 def convert_real_coordinates_into_pixel_tuple_coordinates(array_x_y_z_coord: np.ndarray,
-                                                          x_y_z_spacing, x_y_z_origin, x_y_z_rotation_vectors):
+                                                          x_y_z_spacing, x_y_z_origin,
+                                                          x_y_z_rotation_vectors) -> List[Tuple[int, int]]:
+    """
+    This method will convert the array of patient coordinates into
+    a list of tuple pixel coordinates. Please note, that the z values
+    are ignored because this method is simply used before the produce_mask_from_contour_coord.
+
+    :param array_x_y_z_coord: the array containing all the x y z coordinates
+    :param x_y_z_spacing: spacial dimension of a voxel
+    :param x_y_z_origin: origin of the image in patient coordinates
+    :param x_y_z_rotation_vectors: image axes in the patient coordinates
+
+    :return: the list of the tuples associated to each contour point
+    """
     pixel_coodinates = convert_real_coord_to_pixel_coord(array_x_y_z_coord, x_y_z_spacing, x_y_z_origin,
                                                          x_y_z_rotation_vectors)
     pixel_coords = [(x, y) for x, y, _ in pixel_coodinates]
@@ -110,7 +163,15 @@ def convert_real_coordinates_into_pixel_tuple_coordinates(array_x_y_z_coord: np.
     return pixel_coords
 
 
-def produce_mask_from_contour_coord(coord: list, img_shape):
+def produce_mask_from_contour_coord(coord: List[Tuple[int, int]], img_shape: Tuple[int, int]) -> np.ndarray:
+    """
+    This method creates an array mask from the contour coordinates
+
+    :param coord: the list of all the coutour point coordinates
+    :param img_shape: shape of the corresponding 2d image
+
+    :return: the np.ndarray mask
+    """
     img = Image.new(mode='L', size=img_shape, color=0)
     ImageDraw.Draw(img).polygon(xy=coord, outline=0, fill=1)
     mask = np.array(img).astype(bool)
--- tests/mask_structure_classes_test.py
+++ tests/mask_structure_classes_test.py
@@ -56,8 +56,8 @@ class TestMask(unittest.TestCase):
                                x_y_z_spacing, x_y_z_origin, x_y_z_rotation_vectors, [], "study_folder")
         mask = Mask("roi_name", "observation_label", structure, [])
         mask.add_slices([SliceMask(np.asarray([1, 3, 3]), "uid", 0, mask),
-                        SliceMask(np.asarray([1, 3, 3]), "uid", 0, mask),
-                        SliceMask(np.asarray([1, 3, 3]), "uid", 0, mask)])
+                         SliceMask(np.asarray([1, 3, 3]), "uid", 0, mask),
+                         SliceMask(np.asarray([1, 3, 3]), "uid", 0, mask)])
         self.assertEqual(3, len(mask.list_mask_slices))
 
 
@@ -86,19 +86,59 @@ class TestAllClassesTogether(unittest.TestCase):
         structure = Structures((100, img_shape_2d[0], img_shape_2d[1]),
                                x_y_z_spacing, x_y_z_origin, x_y_z_rotation_vectors, [], "study_folder")
         mask1 = Mask("roi_name1", "observation_label1", structure, [])
+        mask2 = Mask("roi_name2", "observation_label2", structure, [])
+        structure.add_masks([mask1, mask2])
+        self.assertEqual(structure.get_specific_mask("roi_name1", None).observation_label, mask1.observation_label)
+        self.assertEqual(structure.get_specific_mask("roi_name2", None).roi_name, mask2.roi_name)
+        self.assertEqual(structure.get_specific_mask(None, "observation_label1").roi_name, mask1.roi_name)
+        self.assertEqual(structure.get_specific_mask(None, "observation_label2").roi_name, mask2.roi_name)
+        self.assertEqual(structure.get_specific_mask("Roi", "obs"), None)
+
+    def test_list_roi_names_and_observation_labels(self):
+        path_to_image = os.path.join(data_folder, "fictional_study", "series0", "1-01.dcm")
+        img_shape_2d, x_y_z_spacing, x_y_z_origin, x_y_z_rotation_vectors = extract_positionning_informations(
+            path_to_image)
+        structure = Structures((100, img_shape_2d[0], img_shape_2d[1]),
+                               x_y_z_spacing, x_y_z_origin, x_y_z_rotation_vectors, [], "study_folder")
         mask1 = Mask("roi_name1", "observation_label1", structure, [])
-
-    def test_list_roi_names(self):
-        self.assertEqual(False, False)
-
-    def test_list_roi_observation_labels(self):
-        self.assertEqual(False, False)
-
-    def test_list_slice_numbers(self):
-        self.assertEqual(False, False)
+        mask2 = Mask("roi_name2", "observation_label2", structure, [])
+        structure.add_masks([mask1, mask2])
+        self.assertEqual(structure.list_roi_names(), ["roi_name1", "roi_name2"])
+        self.assertEqual(structure.list_roi_observation_labels(), ["observation_label1", "observation_label2"])
 
     def test_get_specific_slice(self):
-        self.assertEqual(False, False)
+        path_to_image = os.path.join(data_folder, "fictional_study", "series0", "1-01.dcm")
+        img_shape_2d, x_y_z_spacing, x_y_z_origin, x_y_z_rotation_vectors = extract_positionning_informations(
+            path_to_image)
+        structure = Structures((100, img_shape_2d[0], img_shape_2d[1]),
+                               x_y_z_spacing, x_y_z_origin, x_y_z_rotation_vectors, [], "study_folder")
+        mask1 = Mask("roi_name1", "observation_label1", structure, [])
+        uid1 = "1.3.6.1.4.1.14519.5.2.1.226742563510968359814545353392980263364"
+        uid2 = "1.3.6.1.4.1.14519.5.2.1.226742563510968359814545353392980263365"
+
+        structure.add_masks(mask1)
+        slice1 = SliceMask(np.asarray([1, 3, 3]), uid1, 1, mask1)
+        slice2 = SliceMask(np.asarray([1, 3, 3]), uid2, 2, mask1)
+        mask1.add_slices([slice1, slice2])
+        self.assertEqual(mask1.get_specific_slice(1).image_uid, slice1.image_uid)
+        self.assertEqual(mask1.get_specific_slice(2).image_uid, slice2.image_uid)
+        self.assertEqual(mask1.get_specific_slice(3), None)
+
+    def test_list_slice_numbers(self):
+        path_to_image = os.path.join(data_folder, "fictional_study", "series0", "1-01.dcm")
+        img_shape_2d, x_y_z_spacing, x_y_z_origin, x_y_z_rotation_vectors = extract_positionning_informations(
+            path_to_image)
+        structure = Structures((100, img_shape_2d[0], img_shape_2d[1]),
+                               x_y_z_spacing, x_y_z_origin, x_y_z_rotation_vectors, [], "study_folder")
+        mask1 = Mask("roi_name1", "observation_label1", structure, [])
+        uid1 = "1.3.6.1.4.1.14519.5.2.1.226742563510968359814545353392980263364"
+        uid2 = "1.3.6.1.4.1.14519.5.2.1.226742563510968359814545353392980263365"
+
+        structure.add_masks(mask1)
+        slice1 = SliceMask(np.asarray([1, 3, 3]), uid1, 1, mask1)
+        slice2 = SliceMask(np.asarray([1, 3, 3]), uid2, 2, mask1)
+        mask1.add_slices([slice1, slice2])
+        self.assertEqual(mask1.list_slice_numbers(), [1, 2])
 
 
 if __name__ == '__main__':
